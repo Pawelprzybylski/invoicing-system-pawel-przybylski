cadfd6b657206114d2899efbbd177c76fd9d7e50
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-08-23 11:20:17 +0200

    Add RestController and Tests

---
+++ src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.futurecollars.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
