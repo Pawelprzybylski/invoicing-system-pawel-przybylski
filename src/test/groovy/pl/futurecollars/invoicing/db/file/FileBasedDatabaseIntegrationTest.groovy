c98c9f30e6b356e9465c53af0975963eb1cbf84d
Pawel-Przybylski <105449778+Pawelprzybylski@users.noreply.github.com>
2022-09-19 11:02:25 +0200

    Add spring dependency injection (#14)
    * Add file's read/write function
    
    * Add file's read/write function
    
    * Delete InMemoryDatabaseTest.groovy
    
    * Add RestController and Tests
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Resolved merge conflict by keeping README.md file.
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Add Spring and RestController
    
    * Delete InvoiceApplicationTest.java
    
    * Revert "Add Spring and RestController"
    
    This reverts commit 23b6c6e114f90eb91f11d26c1fe659fae4e3150b.
    
    * Add Spring Dependency Injection
    
    * jacocoTestCoverageVerification correction value

--- src/test/groovy/pl/futurecollars/invoicing/db/file/FileBasedDatabaseIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/db/file/FileBasedDatabaseIntegrationTest.groovy
@@ -39,4 +39,60 @@ class FileBasedDatabaseIntegrationTest extends AbstractDatabaseTest {
         then:
         2 == Files.readAllLines(dbPath).size()
     }
+
+    def "should return exception when can't save invoice"() {
+
+        given:
+        def db = getDatabaseInstance()
+        Files.deleteIfExists(dbPath)
+
+        when:
+        db.save(TestHelpers.invoice(5))
+
+        then:
+        RuntimeException exception = thrown(RuntimeException)
+        exception.message == "Database failed to save invoice"
+    }
+
+    def "should return exception when can't get invoice"() {
+
+        given:
+        def db = getDatabaseInstance()
+        Files.deleteIfExists(dbPath)
+
+        when:
+        db.getById(2)
+
+        then:
+        RuntimeException exception2 = thrown(RuntimeException)
+        exception2.message == "Database failed to get invoice with id: 2"
+    }
+
+    def "should return exception when can't get all invoices"() {
+
+        given:
+        def db = getDatabaseInstance()
+        Files.deleteIfExists(dbPath)
+
+        when:
+        db.getAll()
+
+        then:
+        RuntimeException exception3 = thrown(RuntimeException)
+        exception3.message == "Failed to read invoices from file"
+    }
+
+    def "should return exception when can't delete invoice"() {
+
+        given:
+        def db = getDatabaseInstance()
+        Files.deleteIfExists(dbPath)
+
+        when:
+        db.delete(82)
+
+        then:
+        RuntimeException exception4 = thrown(RuntimeException)
+        exception4.message == "Failed to delete invoice with id: 82"
+    }
 }
