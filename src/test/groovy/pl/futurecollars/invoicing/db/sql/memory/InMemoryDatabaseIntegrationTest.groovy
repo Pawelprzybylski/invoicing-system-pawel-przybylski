72117a8a1da70b86befcf15d90ac699233864c45
mateuszpyza <mateuszpyza1@wp.pl>
2023-04-02 18:13:19 +0200

    not working yet

--- src/test/groovy/pl/futurecollars/invoicing/db/sql/memory/InMemoryDatabaseIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/db/sql/memory/InMemoryDatabaseIntegrationTest.groovy
@@ -1,7 +1,8 @@
-package pl.futurecollars.invoicing.db.memory
+package pl.futurecollars.invoicing.db.sql.memory
 
 import pl.futurecollars.invoicing.db.AbstractDatabaseTest
 import pl.futurecollars.invoicing.db.Database
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase
 
 class InMemoryDatabaseIntegrationTest extends AbstractDatabaseTest {
 
