05f8fb37eee81143f4a414c09616a63e446fe25d
Pawel-Przybylski <pawel.przybylski1989@gmail.com>
2023-04-15 15:59:00 +0200

    Implement support for companies

--- src/main/java/pl/futurecollars/invoicing/controller/company/CompanyController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/company/CompanyController.java
@@ -3,7 +3,6 @@ package pl.futurecollars.invoicing.controller.company;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
